1934c2448e960d6dad1a8e97b76102ee83f8b21a
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-20 14:11:43 +0100

    Added test to show self-destructed sink.

---
+++ contracts/CanReceive.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.5;
+
+contract CanReceive {
+	function () payable {}
+}
---
+++ test/mortal.js
@@ -0,0 +1,65 @@
+Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
+
+contract('DirectPay', function(accounts) {
+
+    var owner, recipient;
+    var directPay;
+    var right;
+
+    before("should prepare accounts", function () {
+        assert.isAbove(accounts.length, 2, "should have at least 2 accounts");
+        owner = accounts[0];
+        recipient = accounts[1];
+        return Extensions.makeSureAreUnlocked([ owner ]);
+    });
+
+    it("should be possible to send Ether after selfdestruct", function() {
+        var mortal;
+        return CanReceiveMortal.new({ from: owner })
+            .then(created => {
+                mortal = created;
+                return web3.eth.sendTransactionPromise({
+                    from: owner,
+                    to: mortal.address,
+                    value: 1
+                });
+            })
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => {
+                return mortal.kill(owner, { from: owner });
+            })
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => {
+                return web3.eth.getBalancePromise(mortal.address);
+            })
+            .then(balance => {
+                assert.strictEqual(balance.toNumber(), 0, "should be empty");
+                return web3.eth.sendTransactionPromise({
+                    from: owner,
+                    to: mortal.address,
+                    value: 2,
+                    gas: 3000000
+                });
+            })
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => {
+                assert.isBelow(receipt.gasUsed, 3000000, "should not have used all gas");
+                return web3.eth.getBalancePromise(mortal.address);
+            })
+            .then(balance => {
+                assert.strictEqual(balance.toNumber(), 2, "should have received");
+                return mortal.kill(owner, { from: owner });
+            })
+            .then(web3.eth.getTransactionReceiptMined)
+            .then(receipt => {
+                assert.isBelow(receipt.gasUsed, 3000000, "should not have used all gas");
+                return web3.eth.getBalancePromise(mortal.address);
+            })
+            .then(balance => {
+                assert.strictEqual(balance.toNumber(), 2, "should not have been emptied");
+                return mortal.kill(owner, { from: owner });
+            });
+    });
+
+});
